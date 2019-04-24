b252cb1aea96e6c18849753455f37e98b77bd6b7
Assisneto <netodamasceno12@gmail.com>
2019-04-24 08:58:14 -0300

    Book Router

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,10 +3,11 @@ const express = require("express");
 const routes = express.Router();
 
 const AuthorController = require("./controllers/AuthorController");
-
-routes.get("/", (req, res) => res.send("hellou world"));
+const BookController = require("./controllers/BookController");
+routes.get("/", (req, res) => res.send("hello world"));
 
 routes.get("/author", AuthorController.index);
 routes.post("/author", AuthorController.store);
-
+routes.get("/book", BookController.index);
+routes.post("/book", BookController.store);
 module.exports = routes;
