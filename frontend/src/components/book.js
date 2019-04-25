a0679f2e536c8e3c6a0dd6dbb396ac7f64d5a30a
Assisneto <netodamasceno12@gmail.com>
2019-04-25 11:07:46 -0300

    book component

--- frontend/src/components/book.js
+++ frontend/src/components/book.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import api from "./../services/api";
-// import { Container } from './styles';
+import CustomInput from "./customInput";
+import CustomInputForm from "./customInputForm";
 
 export default class BooksBox extends Component {
   constructor() {
@@ -18,13 +19,78 @@ export default class BooksBox extends Component {
           <h1>Books</h1>
         </div>
         <div className="content" id="content">
+          <BooksForm />
           <BooksTable list={this.state.list} />
         </div>
       </div>
     );
   }
 }
+class BooksForm extends Component {
+  constructor() {
+    super();
+    this.state = { title: "", price: "", author: "" };
+  }
+  sendForm = async e => {
+    e.preventDefault();
+    const data = await api.post("/book", {
+      title: this.state.title,
+      price: this.state.price,
+      author: this.state.author
+    });
+    this.setState({ list: data.data, title: "", price: "", author: "" });
+    // data ? PubSub.publish("newList", data.data) : console.log(data);
+  };
+  setTitle = e => {
+    this.setState({ title: e.target.value });
+  };
 
+  setPrice = e => {
+    this.setState({ price: e.target.value });
+  };
+
+  setAuthor = e => {
+    this.setState({ author: e.target.value });
+  };
+  render() {
+    return (
+      <div className="pure-form pure-form-aligned">
+        <form
+          className="pure-form pure-form-aligned"
+          onSubmit={this.sendForm}
+          method="post"
+        >
+          <CustomInput
+            id="title"
+            type="text"
+            name="title"
+            label="title"
+            value={this.state.title}
+            onChange={this.setTitle}
+          />
+          <CustomInput
+            id="price"
+            type="number"
+            name="price"
+            label="price"
+            value={this.state.price}
+            onChange={this.setPrice}
+          />
+
+          <CustomInput
+            id="author"
+            type="text"
+            name="author"
+            label="author"
+            onChange={this.setAuthor}
+            value={this.state.author}
+          />
+          <CustomInputForm label="Gravar" />
+        </form>
+      </div>
+    );
+  }
+}
 class BooksTable extends Component {
   render() {
     return (
@@ -34,6 +100,7 @@ class BooksTable extends Component {
             <tr>
               <th>Title</th>
               <th>Price</th>
+              <th>Author</th>
             </tr>
           </thead>
           <tbody>
@@ -42,6 +109,7 @@ class BooksTable extends Component {
                 <tr key={book._id}>
                   <td>{book.title}</td>
                   <td>{book.price}</td>
+                  <td>{book.Author.name}</td>
                 </tr>
               );
             })}
